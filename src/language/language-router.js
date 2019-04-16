4a1f7222942a14028576128c80ea544854bc1700
David Haugen <haugen.dj@gmail.com>
2019-04-16 16:34:32 -0500

    add several language service methods

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const LanguageService = require('./language-service')
 const { requireAuth } = require('../middleware/jwt-auth')
+const jsonParser = express.json()
 
 const languageRouter = express.Router()
 
@@ -48,8 +49,7 @@ languageRouter
     try{
       const [nextWord] = await LanguageService.getNextWord(
         req.app.get('db'),
-        req.language.id,
-        word_id = 1
+        req.language.id
       )
       res.json({
         nextWord: nextWord.original,
@@ -65,9 +65,42 @@ languageRouter
   })
 
 languageRouter
-  .post('/guess', async (req, res, next) => {
-    // implement me
-    res.send('implement me!')
+  .post('/guess', jsonParser, async (req, res, next) => {
+    const guess = req.body.guess;
+    if(!guess){
+      res.status(400).json({
+        error: `Missing 'guess' in request body`,
+      })
+    }
+
+    try{
+      const words = await LanguageService.getLanguageWords(
+        req.app.get('db'),
+        req.language.id,
+      )
+
+      const [{head}] = await LanguageService.getLanguageHead(
+        req.app.get('db'),
+        req.language.id,
+      )
+
+      console.log(head);
+      const list = await LanguageService.createLinkedList(req.app.get('db'), req.language.id);
+      // console.log(list.head.next.value)
+      const [checkNextWord] = await LanguageService.checkGuess(
+        req.app.get('db'),
+        req.language.id
+      )
+      if(checkNextWord.translation === guess){
+        res.send('you got it right!')
+      } else {
+        res.send('nope');
+      }
+      next()
+    }
+    catch(error){
+      next(error)
+    }
   })
 
 module.exports = languageRouter
--- src/language/language-service.js
+++ src/language/language-service.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const {LinkedList, toArray} = require('../linked-list');
+
+
 const LanguageService = {
   getUsersLanguage(db, user_id) {
     return db
@@ -31,17 +34,61 @@ const LanguageService = {
       .where({ language_id });
   },
 
-  getNextWord(db, language_id, id){
+  // getUserWords(db, language_id){
+
+  // }
+
+  getNextWord(db, language_id){
     return db
       .from('word')
+      .join('language', 'word.id','=','language.head')
       .select(
         'original',
         'language_id',
         'correct_count',
         'incorrect_count'
       )
-      .where({id});
+      .where({language_id});
+  },
+
+  checkGuess(db, language_id){
+    return db
+      .from('word')
+      .join('language', 'word.id','=','language.head')
+      .select(
+        '*'
+      )
+      .where({language_id});
+  },
+
+  getLanguageHead(db, language_id){
+    return db
+      .from('language')
+      .join('word', 'word.language_id','=','language.id')
+      .select('head')
+      .groupBy('head')
+      .where({language_id});
+  },
+
+  getWord(db, language_id){
+    // 
+  },
+
+  createLinkedList(db, language_id){
+    const list = new LinkedList();
+    this.getLanguageHead(
+      db,
+      language_id
+    )
+      // .then(head => head[0].head)
+      .then(head => {list.insertAfter(head);console.log(list.head.value);});
+    // words.forEach(word => {
+    //   list.insertLast(word);
+    // });
+    
+    return list;
   }
+  // createLinkedList()
 };
 
 module.exports = LanguageService;
